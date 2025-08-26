90a93cd698cf8cbd2a171aec1b8e86685c369181
Devon Fulcher <24593113+DevonFulcher@users.noreply.github.com>
2025-08-26 12:05:06 -0500

    Test execute_sql

--- examples/remote_mcp/main.py
+++ examples/remote_mcp/main.py
@@ -1,33 +1,44 @@
 import asyncio
-import json
-
-from mcp.types import TextContent
 
 from remote_mcp.session import session_context
 
 
 async def main():
     async with session_context() as session:
-        available_metrics = await session.call_tool(
-            name="list_metrics",
-            arguments={},
-        )
-        metrics_content = [
-            t for t in available_metrics.content if isinstance(t, TextContent)
-        ]
-        metrics_names = [json.loads(m.text)["name"] for m in metrics_content]
-        print(f"Available metrics: {', '.join(metrics_names)}\n")
-        num_food_orders = await session.call_tool(
-            name="query_metrics",
+        # available_metrics = await session.call_tool(
+        #     name="list_metrics",
+        #     arguments={},
+        # )
+        # metrics_content = [
+        #     t for t in available_metrics.content if isinstance(t, TextContent)
+        # ]
+        # metrics_names = [json.loads(m.text)["name"] for m in metrics_content]
+        # print(f"Available metrics: {', '.join(metrics_names)}\n")
+        # num_food_orders = await session.call_tool(
+        #     name="query_metrics",
+        #     arguments={
+        #         "metrics": [
+        #             "food_orders",
+        #         ],
+        #     },
+        # )
+        # num_food_order_content = num_food_orders.content[0]
+        # assert isinstance(num_food_order_content, TextContent)
+        # print(f"Number of food orders: {num_food_order_content.text}")
+        result = await session.call_tool(
+            name="execute_sql",
             arguments={
-                "metrics": [
-                    "food_orders",
-                ],
+                "sql": "SELECT * FROM { ref('food_orders') }",
             },
         )
-        num_food_order_content = num_food_orders.content[0]
-        assert isinstance(num_food_order_content, TextContent)
-        print(f"Number of food orders: {num_food_order_content.text}")
+        print(result)
+        await session.call_tool(
+            name="execute_sql",
+            arguments={
+                "sql": "SELECT 1 as ok",
+            },
+        )
+        print(result)
 
 
 if __name__ == "__main__":
